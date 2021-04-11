63d0ef8431167ea1cf25411f25df672b39f29007
Nikhil R <nikhilr57@gmail.com>
2021-04-11 13:45:52 +0530

    Updated integration tests

--- app/src/test/java/com/db/demo/trade/api/TradeApplicationIntegrationTest.java
+++ app/src/test/java/com/db/demo/trade/api/TradeApplicationIntegrationTest.java
@@ -5,6 +5,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.time.LocalDate;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -22,18 +23,44 @@ public class TradeApplicationIntegrationTest {
 	private MockMvc mockMvc;
 
 	@Test
-	public void shouldReturnDefaultMessage() throws Exception {
-
-		// this.mockMvc.perform(post("/trades")).andDo(print()).andExpect(status().isOk())
-		// .andExpect(content().string(containsString("Hello World!")));
+	@DisplayName("Add trade valid input")
+	public void testAddTradeWithValidInput() throws Exception {
 
 		ObjectMapper objectMapper = new ObjectMapper();
 
-		TradeRequest tradeReq = new TradeRequest.Builder().setTradeId("T1").setBookId("B1").setCounterPartyId("CP1").setVersion(1L)
-				.setMaturityDate(LocalDate.now()).build();
+		TradeRequest tradeReq = new TradeRequest.Builder().setTradeId("T1").setBookId("B1").setCounterPartyId("CP1")
+				.setVersion(1L).setMaturityDate(LocalDate.now()).build();
 
 		this.mockMvc.perform(post("/trades").content(objectMapper.writeValueAsString(tradeReq))
 				.contentType(MediaType.APPLICATION_JSON).characterEncoding("UTF-8").accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk());
+	}
+
+	@Test
+	@DisplayName("Add trade invalid version")
+	public void testAddTradeWithInvalidVersionInput() throws Exception {
+
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		TradeRequest tradeReq = new TradeRequest.Builder().setTradeId("T1").setBookId("B1").setCounterPartyId("CP1")
+				.setVersion(-1L).setMaturityDate(LocalDate.now()).build();
+
+		this.mockMvc.perform(post("/trades").content(objectMapper.writeValueAsString(tradeReq))
+				.contentType(MediaType.APPLICATION_JSON).characterEncoding("UTF-8").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isBadRequest());
+	}
+
+	@Test
+	@DisplayName("Add trade invalid maturity date")
+	public void testAddTradeWithInvalidMaturityDateInput() throws Exception {
+
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		TradeRequest tradeReq = new TradeRequest.Builder().setTradeId("T1").setBookId("B1").setCounterPartyId("CP1")
+				.setVersion(-1L).setMaturityDate(LocalDate.now().minusDays(1)).build();
+
+		this.mockMvc.perform(post("/trades").content(objectMapper.writeValueAsString(tradeReq))
+				.contentType(MediaType.APPLICATION_JSON).characterEncoding("UTF-8").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isBadRequest());
 	}
 }
